d75cae5e3e25570eb4adc194828c4b123d8fea5f
nicolas <nicolasprodmac@gmail.com>
2020-11-14 18:47:25 -0300

    feat: added api request

---
+++ src/app/services/exchange-rates.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ExchangeRatesResponse } from './payloads/exchange-rates-response';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyExchangeService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getRates(base: string): Observable<ExchangeRatesResponse>{
+    return this.httpClient.get<ExchangeRatesResponse>(`https://api.exchangeratesapi.io/latest?base=${base}`)
+  }
+}
